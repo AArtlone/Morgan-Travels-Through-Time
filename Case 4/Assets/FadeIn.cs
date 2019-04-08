2bdbb01933f1cdaae3e71cc7dec5a374006a92fd
Super-Lovers <n.v.ivanov@st.hanze.nl>
2019-04-08 19:30:38 +0200

    Created logo intro + new refugees

---
+++ Case 4/Assets/FadeIn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    //Attach an Image you want to fade in the GameObject's Inspector
+    public Image m_Image;
+    //Use this to tell if the toggle returns true or false
+    bool m_Fading;
+    // Start is called before the first frame update
+
+
+    void OnGUI()
+    {
+        //Fetch the Toggle's state
+        m_Fading = GUI.Toggle(new Rect(0, 0, 100, 30), m_Fading, "Fade In/Out");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        //If the toggle is false, fade out to nothing (0) the Image with a duration of 2
+        if (m_Fading == false)
+        {
+            m_Image.CrossFadeAlpha(0, 2.0f, false);
+        }
+    }
+    
+    }
---
+++ Case 4/Assets/FadeOut.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{
+    //Attach an Image you want to fade in the GameObject's Inspector
+    public Image m_Image;
+    //Use this to tell if the toggle returns true or false
+    bool m_Fading;
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //If the toggle returns true, fade in the Image
+        if (m_Fading == true)
+        {
+            //Fully fade in Image (1) with the duration of 2
+            m_Image.CrossFadeAlpha(1, 2.0f, false);
+        }
+        //If the toggle is false, fade out to nothing (0) the Image with a duration of 2
+        if (m_Fading == false)
+        {
+            m_Image.CrossFadeAlpha(0, 2.0f, false);
+        }
+    }
+    void OnGUI()
+    {
+        //Fetch the Toggle's state
+        m_Fading = GUI.Toggle(new Rect(0, 0, 100, 30), m_Fading, "Fade In/Out");
+    }
+}
