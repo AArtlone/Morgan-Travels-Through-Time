380a898280f2bbdd31d277240da12c1a48cd5bd4
Super-Lovers <n.v.ivanov@st.hanze.nl>
2019-05-27 16:36:26 +0200

    Completed the npc dialogue log mechanic
    Co-authored-by: AArtlone <artem.stolyha@gmail.com>

--- Case 4/Assets/Scripts/User Interface/Diary/DiaryDialogueLogManager.cs
+++ Case 4/Assets/Scripts/User Interface/Diary/DiaryDialogueLogManager.cs
@@ -1,8 +1,10 @@
 ﻿using LitJson;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiaryDialogueLogManager : MonoBehaviour
 {
@@ -15,6 +17,7 @@ public class DiaryDialogueLogManager : MonoBehaviour
 
     private DiaryManager _diaryManager;
     private List<GameObject> _buttons = new List<GameObject>();
+    private List<List<string>> _pagesOfEntries = new List<List<string>>();
     private int _maxNumberOfButtons = 8;
     private int _currentPage;
 
@@ -22,6 +25,9 @@ public class DiaryDialogueLogManager : MonoBehaviour
     private string _currentPageInLogEntries = "Left";
     private string _selectedNPC;
     private float _currentPositionOfLatestButton;
+    private int _currentLogEntriesPage;
+    private string _lastEntryInLog;
+    private List<string> _exploredEntries = new List<string>();
 
     private void Start()
     {
@@ -109,6 +115,14 @@ public class DiaryDialogueLogManager : MonoBehaviour
 
         JsonData NPCLogs = JsonMapper.ToObject(
             File.ReadAllText(Application.persistentDataPath + "/DialogueLog.json"));
+        foreach (List<string> list in _pagesOfEntries)
+        {
+            //Debug.Log(Environment.NewLine);
+            foreach (string obj in list)
+            {
+                //Debug.Log(obj);
+            }
+        }
 
         for (int i = 0; i < NPCLogs["NPC"].Count; i++)
         {
@@ -118,40 +132,120 @@ public class DiaryDialogueLogManager : MonoBehaviour
                 {
                     if (NPCLogs["NPC"][i]["Log"][j]["Language"].ToString() == SettingsManager.Instance.Language)
                     {
-                        for (int k = 0; k < NPCLogs["NPC"][i]["Log"][j]["Messages"].Count; k++)
+                        //Debug.Log(_currentLogEntryIndex + "|" + NPCLogs["NPC"][i]["Log"][j]["Messages"].Count);
+                        if (_currentLogEntryIndex < NPCLogs["NPC"][i]["Log"][j]["Messages"].Count)
                         {
-                            if (_currentPageInLogEntries == "Left")
+                            _lastEntryInLog = NPCLogs["NPC"][i]["Log"][j]["Messages"][NPCLogs["NPC"][i]["Log"][j]["Messages"].Count - 1].ToString();
+                            ClearSection(LeftPageSection);
+                            ClearSection(RightPageSection);
+                            _currentPositionOfLatestButton = 0;
+                            List<string> newListOfEntries = new List<string>();
+                            bool isDuplicate = false;
+                            //Debug.Log(_currentLogEntryIndex + " out of " + NPCLogs["NPC"][i]["Log"][j]["Messages"].Count);
+                            for (int k = _currentLogEntryIndex; k < NPCLogs["NPC"][i]["Log"][j]["Messages"].Count; k++)
                             {
-                                GameObject newEntry = Instantiate(LogEntryPrefab);
-                                RectTransform entryRect = newEntry.GetComponent<RectTransform>();
-                                newEntry.GetComponent<TextMeshProUGUI>().text = NPCLogs["NPC"][i]["Log"][j]["Messages"][k].ToString();
-                                newEntry.transform.SetParent(LeftPageSection.transform);
-
-                                _currentPositionOfLatestButton += entryRect.sizeDelta.y;
-
-                                if (_currentPositionOfLatestButton + entryRect.sizeDelta.y > _leftPageSectionRect.sizeDelta.y)
+                                GameObject newEntry;
+                                if (_currentPageInLogEntries == "Left")
                                 {
-                                    newEntry.transform.SetParent(RightPageSection.transform);
-                                    _currentPageInLogEntries = "Right";
+                                    newEntry = Instantiate(LogEntryPrefab);
+                                    newEntry.name += _currentLogEntryIndex;
+                                    RectTransform entryRect = newEntry.GetComponent<RectTransform>();
+                                    newEntry.GetComponent<TextMeshProUGUI>().text = NPCLogs["NPC"][i]["Log"][j]["Messages"][k].ToString();
+                                    newEntry.transform.SetParent(LeftPageSection.transform);
+
+                                    _currentPositionOfLatestButton += entryRect.sizeDelta.y + Mathf.Abs(entryRect.rect.y);
+
+                                    if (_currentPositionOfLatestButton > _leftPageSectionRect.sizeDelta.y)
+                                    {
+                                        newEntry.transform.SetParent(RightPageSection.transform);
+                                        _currentPageInLogEntries = "Right";
+
+                                        LayoutRebuilder.ForceRebuildLayoutImmediate(_rightPageSectionRect);
+
+                                        _currentPositionOfLatestButton = entryRect.sizeDelta.y;
+                                    }
+
+                                    newListOfEntries.Add(newEntry.name);
+                                    _exploredEntries.Add(newEntry.GetComponent<TextMeshProUGUI>().text);
+
+                                    LayoutRebuilder.ForceRebuildLayoutImmediate(_leftPageSectionRect);
                                 }
-                            } else if (_currentPageInLogEntries == "Right")
-                            {
-                                GameObject newEntry = Instantiate(LogEntryPrefab);
-                                RectTransform entryRect = newEntry.GetComponent<RectTransform>();
-                                newEntry.GetComponent<TextMeshProUGUI>().text = NPCLogs["NPC"][i]["Log"][j]["Messages"][k].ToString();
-                                newEntry.transform.SetParent(RightPageSection.transform);
-
-                                if (_currentPositionOfLatestButton + entryRect.sizeDelta.y > _rightPageSectionRect.sizeDelta.y)
+                                else if (_currentPageInLogEntries == "Right")
                                 {
-                                    Debug.Log("Done printing.");
-                                    return;
+                                    newEntry = Instantiate(LogEntryPrefab);
+                                    newEntry.name += _currentLogEntryIndex;
+                                    RectTransform entryRect = newEntry.GetComponent<RectTransform>();
+                                    newEntry.GetComponent<TextMeshProUGUI>().text = NPCLogs["NPC"][i]["Log"][j]["Messages"][k].ToString();
+                                    newEntry.transform.SetParent(RightPageSection.transform);
+
+                                    _currentPositionOfLatestButton += entryRect.sizeDelta.y + Mathf.Abs(entryRect.rect.y);
+
+                                    newListOfEntries.Add(newEntry.name);
+                                    _exploredEntries.Add(newEntry.GetComponent<TextMeshProUGUI>().text);
+
+                                    if (_currentPositionOfLatestButton > _rightPageSectionRect.sizeDelta.y)
+                                    {
+                                        _currentPageInLogEntries = "Left";
+                                        //Debug.Log("Done printing.");
+                                        //Debug.Log(_currentLogEntriesPage);
+
+                                        isDuplicate = false;
+                                        foreach (List<string> list in _pagesOfEntries)
+                                        {
+                                            foreach (string obj in list)
+                                            {
+                                                foreach (string entry in newListOfEntries)
+                                                {
+                                                    if (entry == obj)
+                                                    {
+                                                        //Debug.Log("cum");
+                                                        isDuplicate = true;
+                                                        break;
+                                                    }
+                                                }
+                                                break;
+                                            }
+                                        }
+
+                                        if (isDuplicate == false)
+                                        {
+                                            //Debug.Log("AAAAA");
+                                            _pagesOfEntries.Add(newListOfEntries);
+                                        }
+                                        return;
+                                    }
+
+                                    LayoutRebuilder.ForceRebuildLayoutImmediate(_rightPageSectionRect);
+                                }
+
+                                //Debug.Log(_currentPositionOfLatestButton + " | " + _leftPageSectionRect.sizeDelta.y);
+                                _currentLogEntryIndex++;
+                            }
+
+                            isDuplicate = false;
+                            foreach (List<string> list in _pagesOfEntries)
+                            {
+                                foreach (string obj in list)
+                                {
+                                    foreach (string entry in newListOfEntries)
+                                    {
+                                        if (entry == obj)
+                                        {
+                                            isDuplicate = true;
+                                            break;
+                                        }
+                                    }
+                                    break;
                                 }
                             }
 
-                            _currentLogEntryIndex++;
+                            if (isDuplicate == false)
+                            {
+                                //Debug.Log("AAAAA");
+                                _pagesOfEntries.Add(newListOfEntries);
+                            }
+                            break;
                         }
-
-                        break;
                     }
                 }
             }
@@ -163,6 +257,85 @@ public class DiaryDialogueLogManager : MonoBehaviour
         for (int i = 0; i < InterfaceManager.Instance.LogsContainer.transform.childCount; i++)
         {
             InterfaceManager.Instance.LogsContainer.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    public void LoadNewLogEntriesPage(string direction)
+    {
+        _currentPageInLogEntries = "Left";
+        if (direction == "Backwards" || direction == "Backward")
+        {
+            if (_pagesOfEntries.Count > 0  && 
+                _currentLogEntriesPage > 0)
+            {
+                _currentLogEntriesPage--;
+
+                if (_currentLogEntriesPage < 0)
+                {
+                    _currentLogEntriesPage = 0;
+                }
+
+                _currentLogEntryIndex = 0;
+                if (_currentLogEntriesPage > 0)
+                {
+                    for (int i = 0; i < _currentLogEntriesPage; i++)
+                    {
+                        _currentLogEntryIndex += _pagesOfEntries[i].Count;
+                    }
+                }
+                //Debug.Log("Backward" + _currentLogEntryIndex);
+
+                SetupLogEntries(_selectedNPC);
+            }
+        } else if (direction == "Forwards" || direction == "Forward")
+        {
+            if (_pagesOfEntries.Count > 0)
+            {
+                if (_currentLogEntriesPage == 0)
+                {
+                    //Debug.Log("1111111");
+                    _currentLogEntriesPage++;
+
+                    _currentLogEntryIndex = 0;
+                    for (int i = 0; i < _currentLogEntriesPage; i++)
+                    {
+                        _currentLogEntryIndex += _pagesOfEntries[i].Count;
+                    }
+
+                    //Debug.Log("Forward" + _currentLogEntryIndex);
+                    SetupLogEntries(_selectedNPC);
+                }
+                else
+                {
+                    if (_currentLogEntriesPage < _pagesOfEntries.Count)
+                    {
+                        if (_exploredEntries.Contains(_lastEntryInLog) == false)
+                        {
+                            _currentLogEntriesPage++;
+
+                            _currentLogEntryIndex = 0;
+                            for (int i = 0; i < _currentLogEntriesPage; i++)
+                            {
+                                _currentLogEntryIndex += _pagesOfEntries[i].Count;
+                            }
+
+                            //Debug.Log("Forward" + _currentLogEntryIndex);
+                            SetupLogEntries(_selectedNPC);
+                        }
+
+                        _exploredEntries.Clear();
+                    }
+                }
+            }
+            //Debug.Log("Current entries page = " + _currentLogEntriesPage + "|" + _pagesOfEntries.Count);
+        }
+    }
+
+    private void ClearSection(GameObject section)
+    {
+        for (int i = 0; i < section.transform.childCount; i++)
+        {
+            Destroy(section.transform.GetChild(i).gameObject);
         }
     }
 }
